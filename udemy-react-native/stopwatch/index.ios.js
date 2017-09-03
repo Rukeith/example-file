da6c66029914d2799ba85f831838995b6e790c74
Rukeith <fenglan.crazydance@gmail.com>
2017-09-03 21:21:20 +0800

    Finish stopwatch

--- udemy-react-native/stopwatch/index.ios.js
+++ udemy-react-native/stopwatch/index.ios.js
@@ -4,6 +4,7 @@
  * @flow
  */
 
+import moment from 'moment';
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -15,61 +16,99 @@ import {
 
 export default class stopwatch extends Component {
   getInitalState() {
-    timeElapsed: null
+    timeElapsed: '00:00:00',
+    running: false,
+    startTime: null,
+    laps: []
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={[styles.header, this.border('yellow')]}>
-          <View style={[styles.timerWrapper, this.border('red')]}>
-            <Text>
-              00:00:00
+        <View style={[styles.header]}>
+          <View style={[styles.timerWrapper}>
+            <Text style={styles.timer}>
+              {moment(this.state.timeElapsed).format('mm:ss:SS')}
             </Text>
           </View>
-          <View style={[styles.buttonWrapper, this.border('green')]}>
+          <View style={[styles.buttonWrapper]}>
             {this.startStopButton()}
             {this.lapButton()}
           </View>
         </View>
 
-        <View style={[styles.footer, this.border('blue')]}>
+        <View style={[styles.footer]}>
           <Text>
-            I am a list of Laps
+            {this.laps()}
           </Text>
         </View>
       </View>
     );
   }
 
+  laps () {
+    return this.state.laps.map((time, index) => {
+        return <View style={styles.lap}>
+            <Text style={styles.lapText}>
+                Lap #{index + 1}
+            </Text>
+            <Text style={styles.lapText}>
+                {moment(time).format('mm:ss:SS')}
+            </Text>
+        </View>
+    });
+  }
+
   startStopButton () {
+    const style = this.state.running ? styles.stopButton : styles.startButton;
+
     return (
         <TouchableHighlight
           underlayColor="gray"
           onPress={this.handleStartPress}
+          style={[styles.button, style]}
         >
             <Text>
-                Start
+                {this.state.running ? 'Stop' : 'Start'}
             </Text>
         </TouchableHighlight>
     );
   }
 
   handleStartPress () {
-    const startTime = new Date();
-    this.setState({
-      timeElapsed: new Date() - startTime
-    });
+    if (this.state.running) {
+        clearInterval(this.interval);
+        this.setState({ running: false, laps: [] });
+        return;
+    }
+
+    this.setState({ startTime: new Date()  });
+
+    this.interval = setInterval(() => {
+        this.setState({
+            timeElapsed: new Date() - this.state.startTime,
+            running: true
+        });
+    }, 30);
   }
 
   lapButton () {
     return (
-        <View>
+        <TouchableHighlight style={styles.button} underlayColor="gray" onPress={this.handleLapPress}>
             <Text>
                 Lap
             </Text>
-        </View>
+        </TouchableHighlight>
     );
+  }
+
+  handleLapPress () {
+    const lap = this.state.timeElapsed;
+
+    this.setState({
+        startTime: new Date(),
+        laps: this.state.laps.concat([lap])
+    });
   }
 
   border (color) {
@@ -101,6 +140,30 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       justifyContent: 'space-around',
       alignItems: 'center'
+    },
+    timer: {
+      fontSize: 60  
+    },
+    button: {
+        borderWidth: 2,
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    startButton: {
+        borderColor: '#00CC00'
+    },
+    stopButton: {
+        borderColor: '#CC0000'
+    },
+    lap: {
+        justifyContent: 'space-around',
+        flexDirection: 'row'
+    },
+    lapText: {
+        fontSize: 30
     }
 });
 
