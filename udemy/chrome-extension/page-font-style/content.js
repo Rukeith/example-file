7178d959aa1a85cc69f7af75946fab75335fefc5
Rukeith <fenglan.crazydance@gmail.com>
2017-10-22 21:42:15 +0800

    Finish page font style extension

--- udemy/chrome-extension/page-font-style/content.js
+++ udemy/chrome-extension/page-font-style/content.js
@@ -1,8 +1,7 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.todo == "changeColor") {
-        const fontColor = "#" + request.clickedColor;
-        $(".api").css("color", fontColor);
+    if (request.todo === "changeColor") {
+        $(".api").css("color", `#${request.color}`);
     }
 });
--- udemy/chrome-extension/page-font-style/script.js
+++ udemy/chrome-extension/page-font-style/script.js
@@ -1,8 +1,6 @@
 $(() => {
     $("#btnChange").on("click", () => {
         const color = $("#fontColor").val();
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.pageAction.show(tabs[0].id, { todo: "changeColor", clickedColor: color });
-        });
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => chrome.tabs.sendMessage(tabs[0].id, { todo: "changeColor", color }));
     });
 });
