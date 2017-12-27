3f9905222e58f6d6894aaa05a9516810b63edaac
Rukeith <fenglan.crazydance@gmail.com>
2017-12-27 20:05:06 +0800

    Update partice

---
+++ udemy/webpack-2-scratch/src/index.js
@@ -0,0 +1,5 @@
+require('./styles/main.scss');
+import { hello } from './scripts/demo';
+
+hello();
+console.log('Hi from webpack');
---
+++ udemy/webpack-2-scratch/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
