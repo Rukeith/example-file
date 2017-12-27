03511464f499f53d14d4f09ccb096ef5a2908894
Rukeith <fenglan.crazydance@gmail.com>
2017-12-27 20:32:46 +0800

    Seperate css file

--- udemy/webpack-2-scratch/webpack.config.js
+++ udemy/webpack-2-scratch/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -15,12 +16,18 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ]
 };
